c555d766c3c07e5f151fdc245a5d28aa1d3a94cb
Smarthard <th3smartchan@gmail.com>
2019-06-16 20:47:39 +0300

    button appearance fixed

--- src/shikicinema.js
+++ src/shikicinema.js
@@ -1,28 +1,27 @@
 'use strict';
 
-import ShikiAPI from './modules/shikimori-api';
 import Player from './modules/player/player';
 
 
 let player_button = document.createElement('button');
-let div_info = document.body.getElementsByClassName('c-info-right')[0];
-let title = document.body.getElementsByTagName('h1')[0];
+let observer = new MutationObserver(main);
 let shikicinema_player = null;
-
 
 console.log(`shikicinema loaded`);
 
-main();
+observer.observe(document, {childList: true, subtree: true});
 
 async function main() {
 
-    if (div_info) {
+    let div_info = document.querySelector('div.c-info-right');
+    let title = document.body.getElementsByTagName('h1')[0];
+
+    if (!div_info) return ;
+
+    if (div_info && !div_info.contains(player_button)) {
+
         shikicinema_player = new Player();
         player_button.classList.add('b-link_button', 'dark');
-
-        ShikiAPI.isLoggedIn().then(value => {
-            shikicinema_player.inc_button.disabled = !value;
-        });
 
         if (title != null) {
             title = title.innerHTML;
@@ -41,8 +40,6 @@ async function main() {
         } else {
             console.error('Не удалось узнать название аниме!');
         }
-
-
         player_button.addEventListener('click', () => {
             shikicinema_player.show();
         });
