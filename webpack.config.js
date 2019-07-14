c06a282a2577b70f0c1a45143e40aeecd228a7c5
Smarthard <th3smartchan@gmail.com>
2019-07-14 13:17:20 +0300

    copy rules for webpack and version update 0.3.0

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,9 @@
 const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-    context: path.resolve(__dirname, 'src'),
-    "entry": "./shikicinema.js",
+    context: path.resolve(__dirname, ''),
+    "entry": "./src/shikicinema.js",
     "output": {
         "path": path.resolve(__dirname, 'bin'),
         "filename": "shikicinema.min.js",
@@ -11,14 +12,31 @@ module.exports = {
     "module": {
         rules: [
             {
+                "test": /\.js$/,
                 "exclude": "/node_modules/",
                 "loader": "babel-loader"
             },
             {
                 "test": /\.css$/,
                 "loader": 'style!css!'
+            },
+            {
+                "test": /\.json$/,
+                "exclude": /node_modules/,
+                "loader": 'json'
+            },
+            {
+                "test": /\.html$/,
+                "loader": 'html-loader'
             }
         ]
     },
+    plugins: [
+        new CopyPlugin([
+            { from: 'manifest.json' },
+            { from: 'src/shikicinema.css' },
+            { from: 'assets/*.png' }
+        ]),
+    ],
     devtool: process.env.NODE_ENV === 'production' ? 'hidden-source-map' : 'source-map'
 };
