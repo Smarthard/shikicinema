35b8412a3179ad4acd08948c27733d81ac0cc8d3
Smarthard <th3smartchan@gmail.com>
2019-10-15 11:00:08 +0300

    Shikivideo type fields renamed to origins

--- src/ui/src/app/types/shikivideo.ts
+++ src/ui/src/app/types/shikivideo.ts
@@ -1,15 +1,15 @@
 export class Shikivideo {
   readonly id: number;
   readonly url: string;
-  readonly animeId: string;
-  readonly russian: string;
-  readonly english: string;
+  readonly anime_id: string;
+  readonly anime_russian: string;
+  readonly anime_english: string;
   readonly episode: number;
   readonly kind: string;
   readonly language: string;
   readonly quality: string;
   readonly author: string;
-  readonly watches: string;
+  readonly watches_count: string;
   readonly uploader: string;
 
   constructor(obj?: any) {
