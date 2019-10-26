f56276fbfa4bd52d8439aff2834c50f74de24c01
Smarthard <th3smartchan@gmail.com>
2019-10-26 16:58:25 +0300

    environment variables replacement

---
+++ src/ui/extra-webpack.config.js
@@ -0,0 +1,15 @@
+const dotenv = require('dotenv');
+const webpack = require('webpack');
+
+dotenv.config();
+module.exports = {
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV':                     JSON.stringify(process.env.NODE_ENV || 'development'),
+      'process.env.SHIKIVIDEOS_CLIENT_ID':        JSON.stringify(process.env.SHIKIVIDEOS_CLIENT_ID),
+      'process.env.SHIKIVIDEOS_CLIENT_SECRET':    JSON.stringify(process.env.SHIKIVIDEOS_CLIENT_SECRET),
+      'process.env.SHIKIMORI_CLIENT_ID':          JSON.stringify(process.env.SHIKIMORI_CLIENT_ID),
+      'process.env.SHIKIMORI_CLIENT_SECRET':      JSON.stringify(process.env.SHIKIMORI_CLIENT_SECRET)
+    })
+  ]
+};
