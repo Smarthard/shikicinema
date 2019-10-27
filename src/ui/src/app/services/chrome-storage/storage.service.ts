147bb811d7f6005fb5870a61c00806c94eadbdb9
Smarthard <th3smartchan@gmail.com>
2019-10-27 15:08:21 +0300

    storage service fix

--- src/ui/src/app/services/chrome-storage/storage.service.ts
+++ src/ui/src/app/services/chrome-storage/storage.service.ts
@@ -10,13 +10,13 @@ export class StorageService {
 
   static get<T>(name: string, obj: any): Observable<T> {
     return from(new Promise<T>((resolve, reject) => {
-      chrome.storage[name].get({ obj }, (items) => {
+      chrome.storage[name].get(obj, (items) => {
         const err = chrome.runtime.lastError;
 
         if (err) {
           reject(err);
         } else {
-          resolve(items.obj);
+          resolve(items[obj]);
         }
 
       });
@@ -25,7 +25,7 @@ export class StorageService {
 
   static set<T>(name: string, obj: T): Observable<T> {
     return from(new Promise<T>((resolve, reject) => {
-      chrome.storage.sync.set({ obj }, () => {
+      chrome.storage[name].set(obj, () => {
         const err = chrome.runtime.lastError;
 
         if (err) {
