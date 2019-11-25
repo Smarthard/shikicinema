bba62dfaaeab424b07d3e2f6cadeaea1037a3cce
Smarthard <th3smartchan@gmail.com>
2019-11-26 00:55:02 +0300

    simplified uploader.component

--- src/ui/src/app/shared/components/uploader/uploader.component.ts
+++ src/ui/src/app/shared/components/uploader/uploader.component.ts
@@ -1,40 +1,18 @@
-import {Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
-import {Shikimori} from "../../../types/shikimori";
-import {HttpClient} from "@angular/common/http";
+import {Component, Input, OnInit} from '@angular/core';
+import {Shikimori} from '../../../types/shikimori';
 
 @Component({
   selector: 'app-uploader',
   templateUrl: './uploader.component.html',
   styleUrls: ['./uploader.component.css']
 })
-export class UploaderComponent implements OnInit, OnChanges {
+export class UploaderComponent implements OnInit {
 
   @Input()
-  public uploadedByUser: string | number | Shikimori.User;
-  public uploader: Shikimori.User = null;
+  public uploadedByUser: Shikimori.User;
 
-  constructor(
-    private http: HttpClient
-  ) { }
+  constructor() {}
 
   ngOnInit() {}
-
-  ngOnChanges(changes: SimpleChanges): void {
-    const uploader: string | number | Shikimori.User = changes.uploadedByUser.currentValue;
-
-    if (uploader) {
-      if (uploader instanceof Shikimori.User) {
-        this.uploader = uploader;
-      } else {
-        const isUserId: boolean = /\d+/.test(`${uploader}`);
-        this.http.get(`https://shikimori.one/api/users/${ isUserId ? uploader : uploader + '?is_nickname=1' }`)
-          .subscribe(
-            user => {
-              this.uploader = new Shikimori.User(user);
-            }
-          );
-      }
-    }
-  }
 
 }
