07d57835df0953d4b5bc638ad49b91391e6179a0
Smarthard <th3smartchan@gmail.com>
2019-12-05 03:38:29 +0300

    dummy episodes-list.component

--- src/ui/src/app/shared/components/episodes-list/episodes-list.component.ts
+++ src/ui/src/app/shared/components/episodes-list/episodes-list.component.ts
@@ -1,49 +1,33 @@
-import {Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChange, SimpleChanges} from '@angular/core';
-import {ShikivideosService} from '../../../services/shikivideos-api/shikivideos.service';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {SmarthardNet} from '../../../types/smarthard-net';
-import {HttpParams} from '@angular/common/http';
 
 @Component({
   selector: 'app-episodes-list',
   templateUrl: './episodes-list.component.html',
   styleUrls: ['./episodes-list.component.css']
 })
-export class EpisodesListComponent implements OnInit, OnChanges {
+export class EpisodesListComponent implements OnInit {
 
   @Input()
-  public animeId: number;
+  public unique: SmarthardNet.Unique;
 
   @Input()
-  public chosenEp: number;
+  public chosen: SmarthardNet.Shikivideo;
 
   @Output()
   public change: EventEmitter<number> = new EventEmitter<number>();
 
-  public unique: SmarthardNet.Unique;
-  public episodes: string[] = [];
   public limit: number = 30;
 
-  constructor(
-    private videosApi: ShikivideosService
-  ) { }
+  constructor() { }
 
   ngOnInit() {}
 
-  ngOnChanges(changes: SimpleChanges): void {
-    const animeId: SimpleChange = changes.animeId;
-    const params = new HttpParams()
-      .set('anime_id', animeId.currentValue)
-      .set('column', 'url+kind')
-      .set('limit', 'all');
-
-    this.videosApi.getUniqueValues(params)
-      .subscribe((values: SmarthardNet.Unique[]) => {
-        this.unique = values;
-        this.episodes = Object.keys(this.unique);
-      });
+  getUrlsSecondLvlDomain(urls: string[]) {
+    return urls.map(url => url.split('.').slice(-2).join('.'));
   }
 
-  public getUrlsSecondLvlDomain(urls: string[]) {
-    return urls.map(url => url.split('.').slice(-2).join('.'));
+  getEpisodes() {
+    return Object.keys(this.unique);
   }
 }
