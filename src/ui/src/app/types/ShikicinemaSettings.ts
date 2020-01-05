a3726af3307f1b20b5f7360061ff8c4b9ee08d81
Smarthard <th3smartchan@gmail.com>
2020-01-05 19:17:15 +0300

    new settings for enabling/disabling filters (#34)

--- src/ui/src/app/types/ShikicinemaSettings.ts
+++ src/ui/src/app/types/ShikicinemaSettings.ts
@@ -1,6 +1,13 @@
 export class ShikicinemaSettings {
 
   public oldFagApproves: boolean = true;
+  public filters = {
+    enabled: true,
+    language: false,
+    player: true,
+    author: true,
+    quality: true
+  };
 
   constructor(obj?: any) {
     Object.assign(this, obj);
