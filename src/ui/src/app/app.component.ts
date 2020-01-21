80622ef727107c38e80aeea347a3d2509c58544f
Smarthard <th3smartchan@gmail.com>
2020-01-21 22:36:22 +0300

    shikimori token auto refreshing on start solve #10

--- src/ui/src/app/app.component.ts
+++ src/ui/src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AuthService} from './services/auth/auth.service';
+import {timer} from 'rxjs';
+import {switchMap} from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ui';
+export class AppComponent implements OnInit {
+
+  private shikimoriTokenTimer$ = timer(0, 20 * 60 * 60 * 1000);
+
+  constructor(
+    private auth: AuthService
+  ) {}
+
+  ngOnInit(): void {
+    this.shikimoriTokenTimer$
+      .pipe(
+        switchMap(() => this.auth.shikimori$)
+      )
+      .subscribe(token => {
+        if (token.resfresh && token.expired) {
+          this.auth.shikimoriSync();
+        }
+      });
+  }
+
 }
