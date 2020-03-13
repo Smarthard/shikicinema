6a875c1cb8005b45a95be368a12f7f1a829b847a
Smarthard <th3smartchan@gmail.com>
2020-03-13 18:37:05 +0300

    video player skeleton

--- src/ui/src/app/shared/components/video-player/video-player.component.ts
+++ src/ui/src/app/shared/components/video-player/video-player.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {SafeUrl} from "@angular/platform-browser";
+import {interval} from 'rxjs';
 import {SmarthardNet} from '../../../types/smarthard-net';
 
 @Component({
@@ -8,6 +9,8 @@ import {SmarthardNet} from '../../../types/smarthard-net';
   styleUrls: ['./video-player.component.css']
 })
 export class VideoPlayerComponent implements OnInit {
+
+  readonly timeout$ = interval(1000);
 
   @Input()
   public videoUrl: SafeUrl;
