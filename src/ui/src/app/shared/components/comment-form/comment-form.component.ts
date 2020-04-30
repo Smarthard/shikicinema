d5e71c6c76e0fe930c0785f4d1379c624b797b37
Smarthard <th3smartchan@gmail.com>
2020-04-30 14:00:44 +0300

    text format buttons works

--- src/ui/src/app/shared/components/comment-form/comment-form.component.ts
+++ src/ui/src/app/shared/components/comment-form/comment-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-comment-form',
@@ -7,9 +7,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CommentFormComponent implements OnInit {
 
+  static _insertAtCaret(textearea: HTMLTextAreaElement, before?: string, after?: string) {
+    const BEFORE_TEXT = textearea.value.substr(0, textearea.selectionStart);
+    const TEXT = before + textearea.value.substr(textearea.selectionStart, textearea.selectionEnd) + after;
+    const AFTER_TEXT = textearea.value.substr(textearea.selectionEnd, textearea.value.length);
+    textearea.value = BEFORE_TEXT + TEXT + AFTER_TEXT;
+  }
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  bold(textarea: HTMLTextAreaElement) {
+    CommentFormComponent._insertAtCaret(textarea,'[b]', '[/b]');
+  }
+
+  italic(textarea: HTMLTextAreaElement) {
+    CommentFormComponent._insertAtCaret(textarea, '[i]', '[/i]')
+  }
+
+  underline(textarea: HTMLTextAreaElement) {
+    CommentFormComponent._insertAtCaret(textarea, '[u]', '[/u]')
+  }
+
+  strike(textarea: HTMLTextAreaElement) {
+    CommentFormComponent._insertAtCaret(textarea, '[s]', '[/s]')
   }
 
 }
