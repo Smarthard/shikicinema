0b7a8d845b5e7a7bf3a7157bc62edb96157cc234
Smarthard <th3smartchan@gmail.com>
2020-05-01 09:42:36 +0300

    bb-codes insertions save cursor position also bug with text duplication after format fixed

--- src/ui/src/app/shared/components/comment-form/comment-form.component.ts
+++ src/ui/src/app/shared/components/comment-form/comment-form.component.ts
@@ -9,26 +9,36 @@ export class CommentFormComponent implements OnInit {
 
   isSmileysSectionOpen = false;
 
-  static _insertAtCaret(textarea: HTMLTextAreaElement, before?: string, after?: string) {
+  static _insertBeforeAfterCursor(textarea: HTMLTextAreaElement, before = '', after = '') {
+    let newCursorPosition = +textarea.selectionStart + before.length + after.length;
+
     if (textarea.selectionStart || textarea.selectionStart === 0) {
-      const BEFORE_TEXT = textarea.value.substr(0, textarea.selectionStart);
-      const TEXT = before + textarea.value.substr(textarea.selectionStart, textarea.selectionEnd) + after;
-      const AFTER_TEXT = textarea.value.substr(textarea.selectionEnd, textarea.value.length);
+      const BEFORE_TEXT = textarea.value.substring(0, textarea.selectionStart);
+      const TEXT = before + textarea.value.substring(textarea.selectionStart, textarea.selectionEnd) + after;
+      const AFTER_TEXT = textarea.value.substring(textarea.selectionEnd, textarea.value.length);
+
+      newCursorPosition = +textarea.selectionStart + TEXT.length;
       textarea.value = BEFORE_TEXT + TEXT + AFTER_TEXT;
     } else {
       textarea.value += before + after;
     }
+
+    textarea.setSelectionRange(newCursorPosition, newCursorPosition);
   }
 
   static _insertAtCursor(textarea: HTMLTextAreaElement, text: string) {
+    const NEW_CURSOR_POSITION = +textarea.selectionStart + text.length;
+
     if (textarea.selectionStart || textarea.selectionStart === 0) {
-      const BEFORE_TEXT = textarea.value.substr(0, textarea.selectionStart);
-      const AFTER_TEXT = textarea.value.substr(textarea.selectionEnd, textarea.value.length);
+      const BEFORE_TEXT = textarea.value.substring(0, textarea.selectionStart);
+      const AFTER_TEXT = textarea.value.substring(textarea.selectionEnd, textarea.value.length);
 
       textarea.value = BEFORE_TEXT + text + AFTER_TEXT;
     } else {
       textarea.value += text;
     }
+
+    textarea.setSelectionRange(NEW_CURSOR_POSITION, NEW_CURSOR_POSITION);
   }
 
   constructor() { }
@@ -45,23 +55,23 @@ export class CommentFormComponent implements OnInit {
   }
 
   bold(textarea: HTMLTextAreaElement) {
-    CommentFormComponent._insertAtCaret(textarea,'[b]', '[/b]');
+    CommentFormComponent._insertBeforeAfterCursor(textarea,'[b]', '[/b]');
   }
 
   italic(textarea: HTMLTextAreaElement) {
-    CommentFormComponent._insertAtCaret(textarea, '[i]', '[/i]')
+    CommentFormComponent._insertBeforeAfterCursor(textarea, '[i]', '[/i]')
   }
 
   underline(textarea: HTMLTextAreaElement) {
-    CommentFormComponent._insertAtCaret(textarea, '[u]', '[/u]')
+    CommentFormComponent._insertBeforeAfterCursor(textarea, '[u]', '[/u]')
   }
 
   strike(textarea: HTMLTextAreaElement) {
-    CommentFormComponent._insertAtCaret(textarea, '[s]', '[/s]')
+    CommentFormComponent._insertBeforeAfterCursor(textarea, '[s]', '[/s]')
   }
 
   spoiler(textarea: HTMLTextAreaElement) {
-    CommentFormComponent._insertAtCaret(textarea, '[spoiler=спойлер]', '[/spoiler]')
+    CommentFormComponent._insertBeforeAfterCursor(textarea, '[spoiler=спойлер]', '[/spoiler]')
   }
 
   openSmileys() {
