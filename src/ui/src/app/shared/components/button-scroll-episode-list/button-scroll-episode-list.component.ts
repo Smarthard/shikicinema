5b664c02ba4db2c2b608df73a5018013400099f2
Smarthard <th3smartchan@gmail.com>
2020-05-29 00:11:21 +0300

    empty series labels for old episodes list

--- src/ui/src/app/shared/components/button-scroll-episode-list/button-scroll-episode-list.component.ts
+++ src/ui/src/app/shared/components/button-scroll-episode-list/button-scroll-episode-list.component.ts
@@ -1,5 +1,6 @@
 import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {SmarthardNet} from '../../../types/smarthard-net';
+import {Shikimori} from '../../../types/shikimori';
 
 @Component({
   selector: 'app-button-scroll-episode-list',
@@ -7,6 +8,9 @@ import {SmarthardNet} from '../../../types/smarthard-net';
   styleUrls: ['./button-scroll-episode-list.component.css']
 })
 export class ButtonScrollEpisodeListComponent {
+
+  @Input()
+  public anime: Shikimori.Anime;
 
   @Input()
   public unique: SmarthardNet.Unique;
@@ -23,10 +27,37 @@ export class ButtonScrollEpisodeListComponent {
   @Output()
   public change: EventEmitter<number> = new EventEmitter<number>();
 
-  constructor() { }
+  private _episodes: number[] = [];
+
+  constructor() {}
+
+  get maxEpisodeAired() {
+    return this?.anime?.episodes_aired || this?.anime?.episodes || 0;
+  }
+
+  get maxEpisode() {
+    return (this?.maxEpisodeAired < this?.anime.episodes ? this?.anime?.episodes : this?.maxEpisodeAired) || 1;
+  }
+
+  get episodes() {
+    return this._episodes.length === this?.maxEpisode
+      ? this._episodes
+      : new Array<number>(this?.maxEpisode)
+        .fill(0)
+        .map((value, index) => +index + 1)
+  }
 
   getUrlsSecondLvlDomain(urls: string[]) {
     return urls.map(url => url.split('.').slice(-2).join('.'));
+  }
+
+  getEpisodeTooltip(episode) {
+    const isAired = episode <= this?.maxEpisodeAired;
+    const isUploaded = this?.unique[episode];
+
+    return isAired
+      ? (isUploaded ? `Смотреть ${episode} эпизод` : `Эпизод ${episode} еще не загружен`)
+      : `Эпизод ${episode} еще не вышел`;
   }
 
 }
