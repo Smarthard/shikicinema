8862a20908dbab4c9eee78ff0d89186c1f215d7d
Smarthard <th3smartchan@gmail.com>
2022-05-12 19:40:26 +0300

    update dependencies

--- src/ui/extra-webpack.config.js
+++ src/ui/extra-webpack.config.js
@@ -5,7 +5,6 @@ dotenv.config();
 module.exports = {
   plugins: [
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV':                     JSON.stringify(process.env.NODE_ENV || 'development'),
       'process.env.SHIKIVIDEOS_CLIENT_ID':        JSON.stringify(process.env.SHIKIVIDEOS_CLIENT_ID),
       'process.env.SHIKIVIDEOS_CLIENT_SECRET':    JSON.stringify(process.env.SHIKIVIDEOS_CLIENT_SECRET),
       'process.env.SHIKIMORI_CLIENT_ID':          JSON.stringify(process.env.SHIKIMORI_CLIENT_ID),
