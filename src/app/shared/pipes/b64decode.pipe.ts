3fea079f67f1bdd5967af18a066c8786bcbf9942
Smarthard <th3smartchan@gmail.com>
2022-07-05 14:05:03 +0300

    extract base64 encode/decode functions to utils

--- src/app/shared/pipes/b64decode.pipe.ts
+++ src/app/shared/pipes/b64decode.pipe.ts
@@ -1,11 +1,13 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
+import { fromBase64 } from '@app/shared/utils/base64-utils';
+
 @Pipe({
     name: 'b64decode',
     pure: true,
 })
 export class B64decodePipe implements PipeTransform {
     transform(value: string): unknown {
-        return decodeURIComponent(atob(value));
+        return fromBase64(value);
     }
 }
