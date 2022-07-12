d835da7f06ca7070f13dd417a63f534cbb35da81
Smarthard <th3smartchan@gmail.com>
2022-07-12 15:47:45 +0300

    shared base64 module

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -12,21 +12,18 @@ import { TranslocoRootModule } from '@app/transloco-root.module';
 import { AppStateModule } from '@app/store/app-state.module';
 import { ShikimoriClient } from '@app/shared/services/shikimori-client.service';
 import { HeaderComponent } from '@app/core/components/header/header.component';
-import { B64encodePipe } from '@app/shared/pipes/b64encode.pipe';
-import { B64decodePipe } from '@app/shared/pipes/b64decode.pipe';
 import { GoExternalPage } from '@app/core/pages/go-external/go-external.page';
 import { PLATFORM_API_TOKEN, platformApiFactory } from '@app/shared/services/platform-api/platform-api.factory';
 import { ElectronIpcProxyService } from '@app/shared/services/electron-ipc-proxy.service';
 import { SearchbarResultsComponent } from '@app/core/components/searchbar-results/searchbar-results.component';
 import { SkeletonBlockModule } from '@app/shared/components/skeleton-block/skeleton-block.module';
 import { ShikimoriMediaNameModule } from '@app/shared/pipes/shikimori-media-name/shikimori-media-name.module';
+import { Base64Module } from '@app/shared/pipes/base64/base64.module';
 
 const components = [
     HeaderComponent,
     SearchbarResultsComponent,
     GoExternalPage,
-    B64encodePipe,
-    B64decodePipe,
 ];
 
 @NgModule({
@@ -50,6 +47,7 @@ const components = [
         TooltipsModule,
         SkeletonBlockModule,
         ShikimoriMediaNameModule,
+        Base64Module,
     ],
     exports: [
         BrowserModule,
