2c4fecb5c69713a16bbbe0daccdb63ffdde4ae44
Smarthard <th3smartchan@gmail.com>
2024-02-18 17:10:52 +0300

    ignore anime loading if already cached

--- src/app/modules/player/store/effects/player.effects.ts
+++ src/app/modules/player/store/effects/player.effects.ts
@@ -1,13 +1,15 @@
 import {
     Actions,
+    concatLatestFrom,
     createEffect,
     ofType,
 } from '@ngrx/effects';
 import { Injectable } from '@angular/core';
-import { catchError, map } from 'rxjs/operators';
+import { catchError, filter, map } from 'rxjs/operators';
 import { merge, of, switchMap } from 'rxjs';
 
 import { ShikicinemaV1ClientService, ShikimoriClient } from '@app/shared/services';
+import { Store } from '@ngrx/store';
 import {
     addVideosAction,
     findVideosAction,
@@ -15,6 +17,7 @@ import {
     getAnimeInfoFailureAction,
     getAnimeInfoSuccessAction,
 } from '@app/modules/player/store/actions';
+import { selectPlayerAnime, selectPlayerVideos } from '@app/modules/player/store/selectors/player.selectors';
 import { shikicinemaVideoMapper } from '@app/shared/types/shikicinema/v1';
 import { toVideoInfo } from '@app/shared/rxjs';
 
@@ -22,8 +25,10 @@ import { toVideoInfo } from '@app/shared/rxjs';
 export class PlayerEffects {
     findVideos$ = createEffect(() => this.actions$.pipe(
         ofType(findVideosAction),
+        concatLatestFrom(({ animeId }) => this.store$.select(selectPlayerVideos(animeId))),
+        filter(([, videos]) => !videos?.length),
         switchMap(
-            ({ animeId }) => merge(
+            ([{ animeId }]) => merge(
                 this.shikivideos.findAnimes(animeId),
             ).pipe(
                 /* accumulating videos into storage */
@@ -35,13 +40,16 @@ export class PlayerEffects {
 
     getAnimeInfo$ = createEffect(() => this.actions$.pipe(
         ofType(getAnimeInfoAction),
-        switchMap(({ animeId }) => this.shikimori.getAnimeInfo(animeId)),
+        concatLatestFrom(({ animeId }) => this.store$.select(selectPlayerAnime(animeId))),
+        filter(([, anime]) => !anime.id),
+        switchMap(([{ animeId }]) => this.shikimori.getAnimeInfo(animeId)),
         map((anime) => getAnimeInfoSuccessAction({ anime })),
         catchError(() => of(getAnimeInfoFailureAction())),
     ));
 
     constructor(
         private actions$: Actions,
+        private store$: Store,
         private shikivideos: ShikicinemaV1ClientService,
         private shikimori: ShikimoriClient,
     ) {}
