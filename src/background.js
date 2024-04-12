5c545709792150b42edd753b6a43b4759dbb4544
NotaCockroach135 <143725093+NotaCockroach135@users.noreply.github.com>
2024-04-12 22:02:41 +0300

    Update background.js

--- src/background.js
+++ src/background.js
@@ -1,15 +1,18 @@
 
 function run() {
     try {
-        chrome.runtime.onMessage.addListener((request) => {
+        chrome.runtime.onMessage.addListener((request, sender) => {
            if (request.openUrl) {
                chrome.storage.local.get('settings', (obj) => {
                   const settings = obj.settings;
 
                   if (settings && settings.playerTabOpens && settings.playerTabOpens === 'same') {
-                    chrome.tabs.update({ url: request.openUrl });
+                    chrome.tabs.update(sender.tab.id, { url: request.openUrl });
                   } else {
-                    chrome.tabs.create({ url: request.openUrl, index: sender.tab.index + 1, active: true });
+                    chrome.tabs.query({ currentWindow: true, active: true }, (tabs) => {
+                        const currentIndex = tabs[0].index;
+                        chrome.tabs.create({ url: request.openUrl, index: currentIndex + 1, active: true });
+                    });
                   }
                });
            }
