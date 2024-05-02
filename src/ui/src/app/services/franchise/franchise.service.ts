fcfb65738f61aa847f858ed794b192e579e9129a
NotaCockroach135 <143725093+NotaCockroach135@users.noreply.github.com>
2024-05-02 05:58:05 +0300

    Update franchise.service.ts

--- src/ui/src/app/services/franchise/franchise.service.ts
+++ src/ui/src/app/services/franchise/franchise.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import * as yaml from 'yaml';
+import { ShikimoriService } from '../shikimori-api/shikimori.service'
+import { firstValueFrom } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -7,10 +9,47 @@ import * as yaml from 'yaml';
 
 export class FranchiseService {
 
-  async fetchAnimeData(domain: string, animeId: number): Promise<any> {
-    const animeUrl = `${domain}/api/animes/${animeId}`;
-    const animeResponse = await fetch(animeUrl);
-    return animeResponse.json();
+  constructor(private shikimori: ShikimoriService) {}
+
+  async fetchAnimeData(franchise: string): Promise<any> {
+    const domain = await firstValueFrom(this.shikimori.domain$);
+    if (franchise !== null) {
+      const excludedIdsData = await this.fetchData();
+      const excludedIds = this.parseNotAnimeIds(excludedIdsData, franchise);
+      const graphqlUrl = `${domain}/api/graphql`;
+      const query = `
+      {
+        animes(order: aired_on, franchise: "${franchise}", limit: 100, excludeIds: "${excludedIds}", status: "!anons") {
+          id
+          russian
+          kind
+          episodes
+          episodesAired
+          status
+          userRate { status episodes  }
+          airedOn { year }
+          related { relationRu
+            anime {
+              id
+            }
+          } 
+        }
+      }
+    `;
+
+      const response = await fetch(graphqlUrl, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
+        },
+        body: JSON.stringify({ query })
+      });
+
+      const { data } = await response.json();
+      return data
+    }
+
   }
 
   async fetchData(): Promise<any> {
@@ -18,5 +57,23 @@ export class FranchiseService {
     const response = await fetch(url);
     const text = await response.text();
     return yaml.parse(text);
+  }
+
+  async fetchFranchise(animeId: number) {
+    const franchise = await firstValueFrom(this.shikimori.getAnime(animeId))
+    return franchise.franchise
+   }
+
+  private parseNotAnimeIds(data: any[], franchise): number[] {
+    const excludedIdsSet: Set<number> = new Set();
+    for (const entry of data) {
+      if (entry.neko_id === franchise) {
+        const notAnimeIds = entry.filters?.not_anime_ids || [];
+        for (const id of notAnimeIds) {
+          excludedIdsSet.add(id);
+        }
+      }
+    }
+    return [...excludedIdsSet];
   }
 }
