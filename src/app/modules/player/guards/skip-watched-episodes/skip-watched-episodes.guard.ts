46aeafcad5b7c2b9f8b0239b8cf7c053002027f1
Smarthard <th3smartchan@gmail.com>
2024-08-14 11:09:46 +0300

    refactor watched episodes guard

--- src/app/modules/player/guards/skip-watched-episodes/skip-watched-episodes.guard.ts
+++ src/app/modules/player/guards/skip-watched-episodes/skip-watched-episodes.guard.ts
@@ -1,6 +1,6 @@
 import { CanActivateChildFn, Router } from '@angular/router';
 import { Store } from '@ngrx/store';
-import { first, firstValueFrom } from 'rxjs';
+import { first, map, switchMap } from 'rxjs';
 import { inject } from '@angular/core';
 
 import { getAnimeInfoAction } from '@app/modules/player/store/actions';
@@ -8,7 +8,7 @@ import { getLastUnwatchedEpisode } from '@app/modules/player/utils';
 import { selectPlayerAnime } from '@app/modules/player/store/selectors/player.selectors';
 
 
-export const skipWatchedEpisodesGuard: CanActivateChildFn = async (route) => {
+export const skipWatchedEpisodesGuard: CanActivateChildFn = (route) => {
     const animeId = route.paramMap.get('animeId');
     const hasEpisode = !!route.paramMap.get('episode');
 
@@ -20,10 +20,10 @@ export const skipWatchedEpisodesGuard: CanActivateChildFn = async (route) => {
 
         store.dispatch(getAnimeInfoAction({ animeId }));
 
-        const anime$ = store.select(selectPlayerAnime(animeId)).pipe(first((anime) => !!anime?.id));
-        const anime = await firstValueFrom(anime$);
-        const episode = getLastUnwatchedEpisode(anime);
-
-        return router.navigateByUrl(`/player/${animeId}/${episode}`, { replaceUrl: true });
+        return store.select(selectPlayerAnime(animeId)).pipe(
+            first((anime) => !!anime?.id),
+            map((anime) => getLastUnwatchedEpisode(anime)),
+            switchMap((episode) => router.navigate(['/player', animeId, episode], { replaceUrl: true })),
+        );
     }
 };
