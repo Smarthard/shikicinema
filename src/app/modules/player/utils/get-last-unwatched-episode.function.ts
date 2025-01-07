a6145ae0d0399c6e12cf15c9cd814c017a295495
Smarthard <th3smartchan@gmail.com>
2025-01-07 06:13:44 +0300

    fix animes with incorrect status
    (recently changed to ongoing)

--- src/app/modules/player/utils/get-last-unwatched-episode.function.ts
+++ src/app/modules/player/utils/get-last-unwatched-episode.function.ts
@@ -5,7 +5,7 @@ import { getLastAiredEpisode } from '@app/modules/player/utils/get-last-aired-ep
 export function getLastUnwatchedEpisode(anime: AnimeBriefInfoInterface): number {
     const { user_rate: userRate = null } = anime || {};
     const { status = 'planned' } = userRate || {};
-    const lastAiredEpisode = getLastAiredEpisode(anime);
+    const lastAiredEpisode = getLastAiredEpisode(anime) || 1;
     const watchedEpisode = userRate?.episodes || 0;
     const episode = watchedEpisode === lastAiredEpisode && status === 'completed'
         ? 1
