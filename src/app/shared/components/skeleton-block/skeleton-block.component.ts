1b485ab71c231143108dd23936e5efa5e94e3fff
Smarthard <th3smartchan@gmail.com>
2025-06-15 18:43:49 +0300

    migrate skeleton-block to signals

--- src/app/shared/components/skeleton-block/skeleton-block.component.ts
+++ src/app/shared/components/skeleton-block/skeleton-block.component.ts
@@ -1,26 +1,25 @@
 import {
     ChangeDetectionStrategy,
     Component,
-    Input,
+    HostBinding,
     ViewEncapsulation,
+    input,
 } from '@angular/core';
-import { NgStyle } from '@angular/common';
 
 @Component({
     selector: 'app-skeleton-block',
     templateUrl: './skeleton-block.component.html',
     styleUrls: ['./skeleton-block.component.scss'],
     standalone: true,
-    imports: [
-        NgStyle,
-    ],
     changeDetection: ChangeDetectionStrategy.OnPush,
     encapsulation: ViewEncapsulation.None,
 })
 export class SkeletonBlockComponent {
-    @Input({ required: true })
-    height: string;
+    @HostBinding('class.skeleton-block')
+    private skeletonBlockClass = true;
 
-    @Input({ required: true })
-    width: string;
+    height = input.required<string>();
+    width = input.required<string>();
+
+    borderRadius = input<string>(null);
 }
