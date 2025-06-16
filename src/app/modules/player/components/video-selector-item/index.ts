ec139440172bf21d389cc9cf84d7a9cd47dd13a7
Smarthard <th3smartchan@gmail.com>
2025-06-16 19:14:59 +0300

    extract video-selector-item component

---
+++ src/app/modules/player/components/video-selector-item/index.ts
@@ -0,0 +1,1 @@
+export { VideoSelectorItemComponent } from './video-selector-item.component';
--- src/app/modules/player/components/video-selector/video-selector.component.ts
+++ src/app/modules/player/components/video-selector/video-selector.component.ts
@@ -11,27 +11,15 @@ import {
     signal,
     untracked,
 } from '@angular/core';
-import {
-    IonAccordion,
-    IonAccordionGroup,
-    IonButton,
-    IonItem,
-    IonLabel,
-} from '@ionic/angular/standalone';
+import { IonAccordionGroup } from '@ionic/angular/standalone';
 import { NgScrollbar } from 'ngx-scrollbar';
 import { TranslocoService } from '@jsverse/transloco';
-import { UpperCasePipe } from '@angular/common';
 import { toSignal } from '@angular/core/rxjs-interop';
 
 import { FilterByAuthorPipe } from '@app/shared/pipes/filter-by-author/filter-by-author.pipe';
-import { GetColorForSelectablePipe } from '@app/shared/pipes/get-color-for-selectable/get-color-for-selectable.pipe';
-import { GetUrlDomainPipe } from '@app/shared/pipes/get-url-domain/get-url-domain.pipe';
-import { HasQualitiesPipe } from '@app/modules/player/pipes/has-qualities.pipe';
-import { IsSameAuthorPipe } from '@app/shared/pipes/is-same-author/is-same-author.pipe';
-import { IsSameVideoPipe } from '@app/shared/pipes/is-same-video/is-same-video.pipe';
 import { PlayerKindDisplayMode } from '@app/store/settings/types/player-kind-display-mode.type';
-import { SortByDomainPipe } from '@app/shared/pipes/sort-by-domain/sort-by-domain.pipe';
-import { VideoInfoInterface, VideoQualityEnum } from '@app/modules/player/types';
+import { VideoInfoInterface } from '@app/modules/player/types';
+import { VideoSelectorItemComponent } from '@app/modules/player/components/video-selector-item';
 import { cleanAuthorName } from '@app/shared/utils/clean-author-name.function';
 
 
@@ -40,19 +28,9 @@ import { cleanAuthorName } from '@app/shared/utils/clean-author-name.function';
     standalone: true,
     imports: [
         IonAccordionGroup,
-        IonAccordion,
-        IonItem,
-        IonLabel,
-        GetUrlDomainPipe,
-        IonButton,
         FilterByAuthorPipe,
-        GetColorForSelectablePipe,
-        IsSameAuthorPipe,
-        IsSameVideoPipe,
-        SortByDomainPipe,
-        HasQualitiesPipe,
-        UpperCasePipe,
         NgScrollbar,
+        VideoSelectorItemComponent,
     ],
     templateUrl: './video-selector.component.html',
     styleUrl: './video-selector.component.scss',
@@ -65,7 +43,6 @@ export class VideoSelectorComponent {
 
     private readonly transloco = inject(TranslocoService);
 
-    readonly VideoQualityEnum = VideoQualityEnum;
     readonly defaultAuthorName = toSignal<string>(this.transloco.selectTranslate('GLOBAL.VIDEO.AUTHORS.DEFAULT_NAME'));
 
     selected = input<VideoInfoInterface>();
