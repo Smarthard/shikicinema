150108c060e50a960db57bb0fef1c4dcf4469df6
Smarthard <th3smartchan@gmail.com>
2025-06-21 21:02:48 +0300

    extract episode-selector-item component

--- src/app/modules/player/components/episode-selector/episode-selector.component.ts
+++ src/app/modules/player/components/episode-selector/episode-selector.component.ts
@@ -6,21 +6,26 @@ import {
     ViewEncapsulation,
     computed,
     effect,
+    inject,
     input,
     output,
     viewChild,
     viewChildren,
 } from '@angular/core';
-import { IonRippleEffect } from '@ionic/angular/standalone';
 import { NgScrollbar } from 'ngx-scrollbar';
+import { NgxTippyModule } from 'ngx-tippy-wrapper';
+import { TranslocoService } from '@jsverse/transloco';
+import { toSignal } from '@angular/core/rxjs-interop';
 
+import { EpisodeSelectorItemComponent } from '@app/modules/player/components/episode-selector-item';
 
 @Component({
     selector: 'app-episode-selector',
     standalone: true,
     imports: [
-        IonRippleEffect,
         NgScrollbar,
+        NgxTippyModule,
+        EpisodeSelectorItemComponent,
     ],
     templateUrl: './episode-selector.component.html',
     styleUrl: './episode-selector.component.scss',
@@ -31,10 +36,16 @@ export class EpisodeSelectorComponent {
     @HostBinding('class.episode-selector')
     private episodeSelectorClass = true;
 
+    private readonly transloco = inject(TranslocoService);
+
     readonly episodesScrollbar = viewChild<NgScrollbar>('episodesScrollbar');
     readonly episodesEl = viewChildren<ElementRef>('episodeEl');
 
     readonly episodesSkeleton = new Array<number>(50);
+
+    readonly notAiredText = toSignal(
+        this.transloco.selectTranslate('PLAYER_MODULE.PLAYER_PAGE.PLAYER.EPISODE_IS_NOT_AIRED'),
+    );
 
     selected = input.required<number>();
     maxEpisode = input.required<number>();
