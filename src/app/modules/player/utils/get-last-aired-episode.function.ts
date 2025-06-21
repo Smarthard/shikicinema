caca54f17c5a7fd09774084f0090d2a9b27c9b18
Smarthard <th3smartchan@gmail.com>
2025-06-21 22:08:46 +0300

    fix last aired episodes for broken shikimori api states

--- src/app/modules/player/utils/get-last-aired-episode.function.ts
+++ src/app/modules/player/utils/get-last-aired-episode.function.ts
@@ -1,5 +1,7 @@
 import { AnimeBriefInfoInterface } from '@app/shared/types/shikimori/anime-brief-info.interface';
 
 export function getLastAiredEpisode(anime: AnimeBriefInfoInterface): number {
-    return anime?.episodes_aired || anime?.episodes || 0;
+    return anime?.status === 'released'
+        ? anime?.episodes || 0
+        : anime?.episodes_aired || anime?.episodes || 0;
 }
