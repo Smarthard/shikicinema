1874c77681f69953750a0d1a6381d92c6431e594
Smarthard <th3smartchan@gmail.com>
2025-06-23 18:19:15 +0300

    speed up availability calculations a bit

--- src/app/modules/player/pipes/author-availability-warning.pipe.ts
+++ src/app/modules/player/pipes/author-availability-warning.pipe.ts
@@ -15,12 +15,8 @@ export class AuthorAvailabilityWarningPipe implements PipeTransform {
         for (const targetAuthor of authors) {
             const authorVideos = videos?.filter(({ author }) => author === targetAuthor);
             const authorEpisodes = new Set(authorVideos?.map(({ episode }) => episode));
-            const episodesSum = [...authorEpisodes].reduce((acc, episode) => acc + episode, 0);
-            const targetEpSum = (lastAiredEpisode + 1) * (lastAiredEpisode / 2);
 
-            // считаем сумму от первого до последнего вышедшего эпизода
-            // сравниваем с суммой уникальных эпизодов, доступных в видео
-            if (targetEpSum !== episodesSum) {
+            if (authorEpisodes.size !== lastAiredEpisode) {
                 availabilityIssueAuthros.push(targetAuthor);
             }
         }
