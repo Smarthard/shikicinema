fa452af6a62728a06a91b67eb401b9e71fd04b1b
Smarthard <th3smartchan@gmail.com>
2026-02-22 21:54:31 +0300

    add shikimori.io

--- src/app/core/providers/shikimori-domain/default-shikimori-domain.token.ts
+++ src/app/core/providers/shikimori-domain/default-shikimori-domain.token.ts
@@ -1,5 +1,5 @@
 import { InjectionToken } from '@angular/core';
 
-export const DEFAULT_SHIKIMORI_DOMAIN = 'https://shikimori.one';
+export const DEFAULT_SHIKIMORI_DOMAIN = 'https://shikimori.io';
 
 export const DEFAULT_SHIKIMORI_DOMAIN_TOKEN = new InjectionToken<string>('DEFAULT_SHIKIMORI_DOMAIN_TOKEN');
